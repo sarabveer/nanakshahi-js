c9e425628d7946968f42f579ff37c6fc8b84e82a
Sarabveer Singh <me@sarabveer.me>
2018-11-16 17:53:09 -0500

    Add getGregorianFromBikrami API

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const getNanakshahiDate = require( './lib/getNanakshahiDate' )
 const getBikramiDate = require( './lib/getBikramiDate' )
+const getGregorianFromBikrami = require( './lib/getGregorianFromBikrami' )
 
 module.exports = {
   getNanakshahiDate,
   getBikramiDate,
+  getGregorianFromBikrami,
 }

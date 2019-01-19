27cf5ee2f0a6e121a980f11ed48e186f98886db8
Sarabveer Singh <me@sarabveer.me>
2019-01-19 14:19:14 -0500

    Rename to proper terminology

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const getNanakshahiDate = require( './lib/getNanakshahiDate' )
 const getHolidaysForDay = require( './lib/getHolidaysForDay' )
 const getHolidaysForMonth = require( './lib/getHolidaysForMonth' )
 const getMovableHoliday = require( './lib/getMovableHoliday' )
-const getBikramiDate = require( './lib/getBikramiDate' )
-const getGregorianFromBikrami = require( './lib/getGregorianFromBikrami' )
+const getPanchang = require( './lib/getPanchang' )
+const getGregorianFromLunarDate = require( './lib/getGregorianFromLunarDate' )
 
 module.exports = {
   getNanakshahiDate,
   getHolidaysForDay,
   getHolidaysForMonth,
   getMovableHoliday,
-  getBikramiDate,
-  getGregorianFromBikrami,
+  getPanchang,
+  getGregorianFromLunarDate,
 }

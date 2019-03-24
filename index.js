7006233dd8de9467da794463c9fde8d39b437e05
Sarabveer Singh <me@sarabveer.me>
2019-03-24 16:15:17 -0400

    Add getDateFromNanakshahi API and Travis CI tests

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const getNanakshahiDate = require( './lib/getNanakshahiDate' )
+const getDateFromNanakshahi = require( './lib/getDateFromNanakshahi' )
 const getHolidaysForDay = require( './lib/getHolidaysForDay' )
 const getHolidaysForMonth = require( './lib/getHolidaysForMonth' )
 const getMovableHoliday = require( './lib/getMovableHoliday' )
 const getPanchang = require( './lib/getPanchang' )
-const getGregorianFromLunarDate = require( './lib/getGregorianFromLunarDate' )
+const getDateFromLunarTithi = require( './lib/getDateFromLunarTithi' )
 const getTithi = require( './lib/getTithi' )
 
 module.exports = {
   getNanakshahiDate,
+  getDateFromNanakshahi,
   getHolidaysForDay,
   getHolidaysForMonth,
   getMovableHoliday,
   getPanchang,
-  getGregorianFromLunarDate,
+  getDateFromLunarTithi,
   getTithi,
 }

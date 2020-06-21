f1e655a934d13b67e7b64218ef8b95dff091992a
Sarabveer Singh <me@sarabveer.me>
2020-06-20 21:46:08 -0400

    build: update ts definitions

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,27 @@
-export function getNanakshahiDate(gregorianDate: object): object
+export function getNanakshahiDate(gregorianDate?: object = new Date()): object
 
 export function getDateFromNanakshahi(year: number, month: number, date: number): object
 
-export function getHolidaysForDay(gregorianDate?: object): object
+export function getHolidaysForDay(gregorianDate?: object = new Date()): object
 
 export function getHolidaysForMonth(month: number, year?: number): object
 
 export function findMovableHoliday(holiday: string, year?: number): object
 
-export function findBikramiFromDate(date: object, astro?: boolean = true, isJulian?: boolean): object
+export function calculateAstroTimes(date?: object = new Date()): object
+
+export function findBikramiFromDate(date: object, options?: object = { astro: true }): object
 
 export function findDateFromBikramiLunar(
   year: number,
   month: number,
   date: number,
-  paksh?: boolean,
-  leapMonth?: boolean,
-  leapDay?: boolean,
-  astro?: boolean = true,
+  options?: object = { astro: true },
 ): object
 
-export function findDateFromBikramiSolar(year: number, month: number, date: number, astro?: boolean = true): object
+export function findDateFromBikramiSolar(
+  year: number,
+  month: number,
+  date: number,
+  options?: object = { astro: true },
+): object
